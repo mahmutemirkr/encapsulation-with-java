0fb3e4fd84978b745b050f513dd54a823b245a23
Mahmut Emir <77195637+mahmutemirkr@users.noreply.github.com>
2022-11-10 19:12:47 +0300

    school-class-encapsulation

--- src/main/java/studentAndSchoolRegister/School.java
+++ src/main/java/studentAndSchoolRegister/School.java
@@ -1,4 +1,43 @@
 package studentAndSchoolRegister;
 
+import java.util.ArrayList;
+
 public class School {
+
+    private String schoolName;
+
+    private int maxStudentNumbers;
+
+    private ArrayList<Student> students = new ArrayList<>();
+
+    public School(String schoolName, int maxStudentNumbers) {
+        this.schoolName = schoolName;
+        this.maxStudentNumbers = maxStudentNumbers;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public int getMaxStudentNumbers() {
+        return maxStudentNumbers;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public void setMaxStudentNumbers(int maxStudentNumbers) {
+        this.maxStudentNumbers = maxStudentNumbers;
+    }
+
+    public void setStudents(ArrayList<Student> students) {
+        this.students = students;
+    }
+
+
 }
