3a13bcbd7944feb71e8bbb154d3409017951d7a1
Mahmut Emir <77195637+mahmutemirkr@users.noreply.github.com>
2022-11-17 22:04:01 +0300

    StudentRegister-Maintance

--- src/main/java/studentAndSchoolRegister/SchoolRunner.java
+++ src/main/java/studentAndSchoolRegister/SchoolRunner.java
@@ -9,44 +9,9 @@ public class SchoolRunner {
     public static void main(String[] args) {
 
 
-        School school = new School("JavaWithEncapsulation",3);
-        List<Student> studentsOfTheSchool = school.getStudents();
+        StudentRegister studentRegister = new StudentRegister();
+        studentRegister.register();
 
-        int maxStudentsCounter = 1;
-
-        do {
-
-            Scanner scanner = new Scanner(System.in);
-
-            System.out.print(maxStudentsCounter + ".Student Name:");
-            String studentName = scanner.nextLine();
-
-            System.out.print(maxStudentsCounter + ".Student Surname:");
-            String studentSurName = scanner.nextLine();
-
-            System.out.print(maxStudentsCounter + ".Student Age:");
-            int studentAge = scanner.nextInt();
-
-            System.out.println("----------------------------------------");
-
-
-            try{
-
-                Student student = new Student(studentName,studentSurName,studentAge);
-                studentsOfTheSchool.add(student);
-                maxStudentsCounter++;
-
-            } catch (Exception e){
-
-                System.out.println(e.getMessage());
-
-            }
-
-        }while (maxStudentsCounter<=school.getMaxStudentNumbers());
-
-        for(Student student: school.getStudents()){
-            System.out.println("Students = " + student);
-        }
 
     }
 
---
+++ src/main/java/studentAndSchoolRegister/StudentRegister.java
@@ -0,0 +1,49 @@
+package studentAndSchoolRegister;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class StudentRegister {
+
+    public void register(){
+
+        School school = new School("JavaWithEncapsulation",3);
+        List<Student> studentsOfTheSchool = school.getStudents();
+
+        int maxStudentsCounter = 1;
+
+        do {
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print(maxStudentsCounter + ".Student Name:");
+            String studentName = scanner.nextLine();
+
+            System.out.print(maxStudentsCounter + ".Student Surname:");
+            String studentSurName = scanner.nextLine();
+
+            System.out.print(maxStudentsCounter + ".Student Age:");
+            int studentAge = scanner.nextInt();
+
+            System.out.println("----------------------------------------");
+
+
+            try{
+
+                Student student = new Student(studentName,studentSurName,studentAge);
+                studentsOfTheSchool.add(student);
+                maxStudentsCounter++;
+
+            } catch (Exception e){
+
+                System.out.println(e.getMessage());
+
+            }
+
+        }while (maxStudentsCounter<=school.getMaxStudentNumbers());
+
+        for(Student student: school.getStudents()){
+            System.out.println("Students = " + student);
+        }
+    }
+}
